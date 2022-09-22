8ed26f589616c1c02eb53d8f91b9ecb7cea110a5
Tonni Das Jui <tonnijuicse@gmail.com>
2022-09-22 04:47:33 -0500

    Add service to handle product APIs

---
+++ backend/src/main/java/edu/baylor/cs/beargo/service/MyService.java
@@ -0,0 +1,69 @@
+package edu.baylor.cs.beargo.service;
+
+import edu.baylor.cs.beargo.model.Product;
+import edu.baylor.cs.beargo.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public void populate() {
+        for (int i = 1; i <= 3; i++) {
+            Product product = new Product();
+            product.setName("product-" + i);
+            product.setDescription("description-" + i);
+            product.setWeight(20.0 + i);
+            productRepository.save(product);
+        }
+    }
+
+    public List<Product> getProducts(String name) {
+        if (name == null) {
+            return productRepository.findAll();
+        } else {
+            return productRepository.findByName(name);
+        }
+    }
+
+    public Product getProductById(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            return product.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No product record exist for given id");
+        }
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        product.setId(id);
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No product record exist for given id");
+        }
+    }
+}
