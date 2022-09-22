5a3230570514315d5fe1d89f3b385451af06165f
Tonni Das Jui <tonnijuicse@gmail.com>
2022-09-22 04:46:25 -0500

    Add product entity and repository

---
+++ backend/src/main/java/edu/baylor/cs/beargo/model/Product.java
@@ -0,0 +1,26 @@
+package edu.baylor.cs.beargo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double weight;
+}
---
+++ backend/src/main/java/edu/baylor/cs/beargo/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package edu.baylor.cs.beargo.repository;
+
+import edu.baylor.cs.beargo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByName(String name);
+
+    List<Product> findAllByOrderByWeightAsc();
+
+    List<Product> findAllByOrderByWeightDesc();
+
+    @Query(value = "SELECT * FROM PRODUCT WHERE WEIGHT > ?1", nativeQuery = true)
+    List<Product> findByMinimumWeight(int weight);
+}
