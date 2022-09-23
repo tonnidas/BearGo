6ea9dbe4c6a0edb7002d8915cc60bec6a59828cc
Tonni Das Jui <tonnijuicse@gmail.com>
2022-09-23 04:44:29 -0500

    Update api paths

--- backend/src/main/java/edu/baylor/cs/beargo/controller/MyController.java
+++ backend/src/main/java/edu/baylor/cs/beargo/controller/MyController.java
@@ -11,42 +11,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api")
 public class MyController {
     @Autowired
     MyService service;
 
+    // TODO: remove this in production
     @GetMapping("/populate")
     public HttpStatus populate() {
         service.populate();
         return HttpStatus.OK;
     }
 
-    @GetMapping
+    @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(@RequestParam(required = false) String name) {
         List<Product> products = service.getProducts(name);
         return new ResponseEntity<>(products, new HttpHeaders(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/products/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) {
         Product product = service.getProductById(id);
         return new ResponseEntity<>(product, new HttpHeaders(), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product createdProduct = service.createProduct(product);
         return new ResponseEntity<>(createdProduct, new HttpHeaders(), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/products/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
         Product updatedProduct = service.updateProduct(id, product);
         return new ResponseEntity<>(updatedProduct, new HttpHeaders(), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/products/{id}")
     public HttpStatus deleteProduct(@PathVariable("id") Long id) {
         service.deleteProduct(id);
         return HttpStatus.OK;
