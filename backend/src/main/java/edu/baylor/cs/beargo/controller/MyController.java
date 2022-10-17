5c5afa331f8e8d650bbf70562bedd3ca14f457c4
Tonni Das Jui <tonnijuicse@gmail.com>
2022-10-17 17:13:26 -0500

    Removed empty headers from controller response

--- backend/src/main/java/edu/baylor/cs/beargo/controller/MyController.java
+++ backend/src/main/java/edu/baylor/cs/beargo/controller/MyController.java
@@ -3,7 +3,6 @@ package edu.baylor.cs.beargo.controller;
 import edu.baylor.cs.beargo.model.Product;
 import edu.baylor.cs.beargo.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,38 +19,38 @@ public class MyController {
     // TODO: remove this in production
     @PostMapping("/populate")
     @PreAuthorize("hasRole('ADMIN')")
-    public HttpStatus populate() {
+    public ResponseEntity<String> populate() {
         service.populate();
-        return HttpStatus.OK;
+        return new ResponseEntity<>("Data populated", HttpStatus.OK);
     }
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(@RequestParam(required = false) String name) {
         List<Product> products = service.getProducts(name);
-        return new ResponseEntity<>(products, new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) {
         Product product = service.getProductById(id);
-        return new ResponseEntity<>(product, new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
     @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product createdProduct = service.createProduct(product);
-        return new ResponseEntity<>(createdProduct, new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(createdProduct, HttpStatus.OK);
     }
 
     @PutMapping("/products/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
         Product updatedProduct = service.updateProduct(id, product);
-        return new ResponseEntity<>(updatedProduct, new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 
     @DeleteMapping("/products/{id}")
-    public HttpStatus deleteProduct(@PathVariable("id") Long id) {
+    public ResponseEntity<String> deleteProduct(@PathVariable("id") Long id) {
         service.deleteProduct(id);
-        return HttpStatus.OK;
+        return new ResponseEntity<>("Product deleted", HttpStatus.OK);
     }
 }
