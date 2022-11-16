f16ef822baa69a8f720984a733d80ed05f3114a1
agmislam1 <104467848+agmislam1@users.noreply.github.com>
2022-11-16 01:51:23 -0600

    Message Service

--- backend/src/main/java/edu/baylor/cs/beargo/service/MessageService.java
+++ backend/src/main/java/edu/baylor/cs/beargo/service/MessageService.java
@@ -1,2 +1,24 @@
-package edu.baylor.cs.beargo.service;public class MessageService {
+package edu.baylor.cs.beargo.service;
+
+import edu.baylor.cs.beargo.model.Message;
+import edu.baylor.cs.beargo.repository.MessageRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageService {
+
+    @Autowired
+    MessageRepository msgRepo;
+
+    public Message saveMsg(Message m){
+        
+        Message savedMsg = msgRepo.save(m);
+        return savedMsg;
+
+    }
 }
