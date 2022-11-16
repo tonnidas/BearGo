1843fa5c452d0ba8d4a2a6fa2c122f317b33f8d2
agmislam1 <104467848+agmislam1@users.noreply.github.com>
2022-11-16 01:59:19 -0600

    Message Api added for retrieving all msg

--- backend/src/main/java/edu/baylor/cs/beargo/controller/MessageController.java
+++ backend/src/main/java/edu/baylor/cs/beargo/controller/MessageController.java
@@ -1,13 +1,27 @@
 package edu.baylor.cs.beargo.controller;
 
+import edu.baylor.cs.beargo.model.Message;
+
 import edu.baylor.cs.beargo.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/Message")
 public class MessageController {
     @Autowired
     MessageService msgService;
+
+    @GetMapping("/{uid}")
+    public ResponseEntity<List<Message>> getNotification(@PathVariable("uid") Long uid) {
+        List<Message> msgList = msgService.getAllmsg(uid);
+        return new ResponseEntity<>(msgList, HttpStatus.OK);
+    }
 }
