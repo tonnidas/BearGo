47b2cd325eebc36ef9d74a8db493a18c823b206a
agmislam1 <104467848+agmislam1@users.noreply.github.com>
2022-11-16 02:08:11 -0600

    Message Api added with authentication

--- backend/src/main/java/edu/baylor/cs/beargo/controller/MessageController.java
+++ backend/src/main/java/edu/baylor/cs/beargo/controller/MessageController.java
@@ -2,10 +2,12 @@ package edu.baylor.cs.beargo.controller;
 
 import edu.baylor.cs.beargo.model.Message;
 
+import edu.baylor.cs.beargo.model.User;
 import edu.baylor.cs.beargo.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,19 @@ public class MessageController {
     MessageService msgService;
 
     @GetMapping("/{uid}")
-    public ResponseEntity<List<Message>> getNotification(@PathVariable("uid") Long uid) {
+    public ResponseEntity<List<Message>> getMsgList(@AuthenticationPrincipal User user) {
+
+        Long uid = user.getId();
         List<Message> msgList = msgService.getAllmsg(uid);
         return new ResponseEntity<>(msgList, HttpStatus.OK);
     }
+
+    @GetMapping("/{uid}")
+    public ResponseEntity<Message> saveMsg(@AuthenticationPrincipal User user, Message m) {
+
+        Message msg = msgService.saveMsg(m);
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+
 }
