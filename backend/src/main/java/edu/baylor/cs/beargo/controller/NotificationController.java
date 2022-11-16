1742db7cf192c7d6618f2420ed30cfb46b40554d
agmislam1 <104467848+agmislam1@users.noreply.github.com>
2022-11-16 02:08:42 -0600

    Notification api added

--- backend/src/main/java/edu/baylor/cs/beargo/controller/NotificationController.java
+++ backend/src/main/java/edu/baylor/cs/beargo/controller/NotificationController.java
@@ -3,8 +3,12 @@ package edu.baylor.cs.beargo.controller;
 
 import edu.baylor.cs.beargo.model.Notification;
 import edu.baylor.cs.beargo.model.Product;
+import edu.baylor.cs.beargo.model.User;
+import edu.baylor.cs.beargo.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +20,14 @@ import java.util.List;
 @RequestMapping("/api/notification")
 public class NotificationController {
 
-    @GetMapping("/{uid}")
-    public ResponseEntity<List<Notification>> getNotification(@PathVariable("uid") Long uid) {
+    @Autowired
+    NotificationService notificationService;
 
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<List<Notification>> getNotification(@AuthenticationPrincipal User user) {
+
+        Long uid = user.getId();
+        List<Notification> notificationList = notificationService.getNotification(uid);
+        return new ResponseEntity<>(notificationList, HttpStatus.OK);
     }
 }
