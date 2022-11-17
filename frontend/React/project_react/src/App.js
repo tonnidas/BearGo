1d9e5165778a16e73fba7f67a89e66746a146e9b
Maisha Binte Rashid <rashid.maisha05@gmail.com>
2022-11-16 20:32:32 -0600

    App.js modified

--- frontend/React/project_react/src/App.js
+++ frontend/React/project_react/src/App.js
@@ -1,5 +1,6 @@
 import FrontPage from "./FrontPage/FrontPage";
 import CreatePost from "./CreatePost/CreatePost";
+import ContactSender from "./ContactSender/ContactSender";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -7,8 +8,8 @@ function App() {
     <BrowserRouter>
      <Routes>
       <Route path="/" element={<FrontPage />} />
-          <Route path="/createPost" element={<CreatePost />} />
-          
+      <Route path="/createPost" element={<CreatePost />} />
+      <Route path="/contactSender" element={<ContactSender />} />
         </Routes> 
         </BrowserRouter>
   //  <FrontPage/>
