216e177cade6d9191cb331c52ed3db4b71f05944
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-11-17 17:00:48 -0600

    Deleted .css files for register page and added .jsx

--- frontend/React/project_react/src/Register/Register.jsx
+++ frontend/React/project_react/src/Register/Register.jsx
@@ -1,11 +1,50 @@
 import "./Register.css"
 
+import logo_white from '../images/logo-white.svg';
 
 export default function Register() {
     let url = ""
     return(
         <div>
-            
+            <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-md-5">
+                <form className="user-form">
+                    <div className="text-center">
+                        <img src={logo_white} alt="" />
+                    </div>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input type="text" className="form-control" placeholder="Jhon" />
+                    </div>
+                    <div className="form-group">
+                        <label>Phone</label>
+                        <input type="text" className="form-control" placeholder="(XXX)-XXX-XXXX)" />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="text" className="form-control" placeholder="jhon@xyz.com" />
+                    </div>
+                    <div className="form-group">
+                        <label>Social Security Number</label>
+                        <input type="text" className="form-control" placeholder="XXX)-XXX-XXXX" />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="12345" />
+                    </div>
+
+
+                    <button type="button" onclick="showModal('mymodal')"
+                        className="common-btn btn-primary">Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+   
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
         </div>
     )
 }
