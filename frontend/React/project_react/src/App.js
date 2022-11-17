7dc6995e7ad32e3f082c8e2e063fbdecf983ff14
Maisha Binte Rashid <rashid.maisha05@gmail.com>
2022-11-16 19:42:37 -0600

    route link changed

--- frontend/React/project_react/src/App.js
+++ frontend/React/project_react/src/App.js
@@ -1,9 +1,18 @@
 import FrontPage from "./FrontPage/FrontPage";
 import CreatePost from "./CreatePost/CreatePost";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-   <FrontPage/>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<FrontPage />} />
+          <Route path="/createPost" element={<CreatePost />} />
+          
+        </Routes> 
+        </BrowserRouter>
+  //  <FrontPage/>
+   
   );
 }
 
