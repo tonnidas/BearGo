98019025edaba02a9d6dca8d3d9bee55c01cb6b3
Maisha Binte Rashid <rashid.maisha05@gmail.com>
2022-11-17 11:47:42 -0600

    Components added

--- frontend/React/project_react/src/App.js
+++ frontend/React/project_react/src/App.js
@@ -4,16 +4,17 @@ import ContactSender from "./ContactSender/ContactSender";
 import Register from "./Register/Register";
 import Login from "./Login/Login";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import urlPaths from './urlPaths';
 
 function App() {
   return (
     <BrowserRouter>
      <Routes>
-      <Route path="/" element={<FrontPage />} />
-      <Route path="/createPost" element={<CreatePost />} />
-      <Route path="/contactSender" element={<ContactSender />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+     <Route path={urlPaths.home} element={<FrontPage />} />
+      <Route path={urlPaths.createPost} element={<CreatePost />} />
+      <Route path={urlPaths.contactSender} element={<ContactSender />} />
+      <Route path={urlPaths.register} element={<Register />} />
+      <Route path={urlPaths.login} element={<Login />} />
         </Routes> 
         </BrowserRouter>
   //  <FrontPage/>
---
+++ frontend/React/project_react/src/urlPaths.js
@@ -0,0 +1,12 @@
+const urlPaths = {
+    home: '/',
+    contactSender: '/contactSender',
+    createPost: '/createPost',
+    message: '/message',
+    settings: '/settings',
+    myBlog: '/myBlog',
+    register: '/register',
+    login: '/login'
+  };
+  
+  export default urlPaths;
