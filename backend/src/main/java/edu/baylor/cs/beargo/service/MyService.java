856e20afc18bd2d8f9ab989501e337b99c928cb4
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-11-17 22:53:46 -0600

    Added data populate for BlogPost and ReviewAndRating.

--- backend/src/main/java/edu/baylor/cs/beargo/service/MyService.java
+++ backend/src/main/java/edu/baylor/cs/beargo/service/MyService.java
@@ -1,13 +1,19 @@
 package edu.baylor.cs.beargo.service;
 
+import edu.baylor.cs.beargo.model.BlogPost;
 import edu.baylor.cs.beargo.model.Product;
+import edu.baylor.cs.beargo.model.ReviewAndRating;
 import edu.baylor.cs.beargo.model.User;
+import edu.baylor.cs.beargo.repository.BlogPostRepository;
 import edu.baylor.cs.beargo.repository.ProductRepository;
+import edu.baylor.cs.beargo.repository.ReviewAndRatingRepository;
 import edu.baylor.cs.beargo.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @AllArgsConstructor
@@ -19,6 +25,12 @@ public class MyService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    BlogPostRepository blogPostRepository;
+
+    @Autowired
+    ReviewAndRatingRepository reviewAndRatingRepository;
 
     public void populate() {
         for (int i = 1; i <= 3; i++) {
@@ -42,7 +54,28 @@ public class MyService {
             user.setIsAdmin(true);
             userRepository.save(user);
         }
+        for (int i = 1; i <= 5; i++) {
+            BlogPost blogPost = new BlogPost();
+            blogPost.setDescription("Blog Post Description " + i);
+            blogPost.setPostedTime(LocalDate.now());
+            blogPost.setLastEditedTime(blogPost.getPostedTime());
+            // It depends on the user loop. There must be some user with this user name.
+            User user = userRepository.findByUsername("user"+((i % 2) + 11)+"@beargo.com").get();
+            blogPost.setPostedBy(user);
+            blogPostRepository.save(blogPost);
+        }
 
-
+        for (int i = 1; i <= 5; i++) {
+            ReviewAndRating reviewAndRating = new ReviewAndRating();
+            reviewAndRating.setReview("This person is good " + i);
+            reviewAndRating.setRating(5);
+            reviewAndRating.setReviewTime(LocalDate.now());
+            // It depends on the user loop. There must be some user with this user name.
+            User user1 = userRepository.findByUsername("user"+(i * 2 + 9)+"@beargo.com").get();
+            User user2 = userRepository.findByUsername("user"+(i * 2 + 10)+"@beargo.com").get();
+            reviewAndRating.setReviewedBy(user1);
+            reviewAndRating.setReviewedTo(user2);
+            reviewAndRatingRepository.save(reviewAndRating);
+        }
     }
 }
