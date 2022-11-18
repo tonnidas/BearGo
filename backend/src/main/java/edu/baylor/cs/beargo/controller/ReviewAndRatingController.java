add9b527cca3ad032db808bb0fcd3d2cbfbc3d4d
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-11-17 22:53:20 -0600

    Added ReviewAndRating controller.

---
+++ backend/src/main/java/edu/baylor/cs/beargo/controller/ReviewAndRatingController.java
@@ -0,0 +1,24 @@
+package edu.baylor.cs.beargo.controller;
+
+import edu.baylor.cs.beargo.model.ReviewAndRating;
+import edu.baylor.cs.beargo.service.ReviewAndRatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reviewAndRating")
+public class ReviewAndRatingController {
+
+    @Autowired
+    ReviewAndRatingService reviewAndRatingService;
+
+    @GetMapping("/getAllReviewAndRating")
+    public ResponseEntity<List<ReviewAndRating>> getAllReviewAndRating() {
+        List<ReviewAndRating> reviewAndRatings = reviewAndRatingService.getReviewAndRatings();
+        return new ResponseEntity<>(reviewAndRatings, HttpStatus.OK);
+    }
+}
