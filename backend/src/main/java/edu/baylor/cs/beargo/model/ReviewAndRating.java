1b41d348fcbc454ccf81a8da2001d648f6c948c9
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-11-17 22:51:14 -0600

    Added some constraints in ReviewAndRating class.

--- backend/src/main/java/edu/baylor/cs/beargo/model/ReviewAndRating.java
+++ backend/src/main/java/edu/baylor/cs/beargo/model/ReviewAndRating.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.time.LocalDate;
 
 @Getter
@@ -24,6 +26,8 @@ public class ReviewAndRating {
     private String review;
 
     @Column
+    @Min(value=0, message="Rating must be equal or greater than 0")
+    @Max(value=5, message="Rating must be equal or less than 5")
     private Integer rating;
 
     @Column(nullable = false)
