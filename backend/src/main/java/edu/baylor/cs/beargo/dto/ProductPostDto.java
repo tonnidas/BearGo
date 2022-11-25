354c772a31d7e9c7f8e2afd9a811c4c9ba387931
Tonni Das Jui <tonnijuicse@gmail.com>
2022-11-25 16:29:02 -0600

    Use model mapper to convert entity to dto

--- backend/src/main/java/edu/baylor/cs/beargo/dto/ProductPostDto.java
+++ backend/src/main/java/edu/baylor/cs/beargo/dto/ProductPostDto.java
@@ -1,44 +1,47 @@
 package edu.baylor.cs.beargo.dto;
 
-import edu.baylor.cs.beargo.model.*;
+import edu.baylor.cs.beargo.model.Address;
+import edu.baylor.cs.beargo.model.Product;
+import edu.baylor.cs.beargo.model.ProductPost;
+import edu.baylor.cs.beargo.model.ProductPostComment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.modelmapper.ModelMapper;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductPostDetails {
-    ProductPost productPost;
-    Product product;
-    Address source;
-    Address destination;
-    Contract contract;
-    User sender;
-    User traveller;
+public class ProductPostDto {
+    private Long id;
+    private String description;
+    private String imageFilePath;
+    private LocalDate expectedDeliveryDate;
+    private Address source;
+    private Address destination;
 
-    public ProductPostDetails(ProductPost productPost) {
-        this.productPost = productPost;
-        this.product = productPost.getProduct();
-        this.source = productPost.getSource();
-        this.destination = productPost.getDestination();
-        this.contract = productPost.getContract();
-        this.sender = productPost.getContract().getSender();
-        this.traveller = productPost.getContract().getTraveler();
-    }
+    private Product product;
+    private ContractDto contract;
 
-    public static List<ProductPostDetails> convertProductPostList(List<ProductPost> productPosts) {
-        List<ProductPostDetails> productPostDetails = new ArrayList<>();
+    private Set<ProductPostComment> comments = new HashSet<>();
+
+    public static List<ProductPostDto> getProductPostDtoList(List<ProductPost> productPosts) {
+        ModelMapper modelMapper = new ModelMapper();
+        List<ProductPostDto> productPostDtoList = new ArrayList<>();
 
         for (ProductPost productPost : productPosts) {
-            productPostDetails.add(new ProductPostDetails(productPost));
+            ProductPostDto productPostDto = modelMapper.map(productPost, ProductPostDto.class);
+            productPostDtoList.add(productPostDto);
         }
 
-        return productPostDetails;
+        return productPostDtoList;
     }
 }
