c4f883b025626ead17b531db9a48051dc2877016
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-11-28 22:37:42 -0600

    Added getContractFrequency API from frontend and required design to show the data received from this API.

--- frontend/React/project_react/src/Profile/ReviewAndRatingPage.jsx
+++ frontend/React/project_react/src/Profile/ReviewAndRatingPage.jsx
@@ -6,6 +6,21 @@ import { useNavigate } from "react-router-dom";
 import urlPaths from '../urlPaths';
 import StarRatings from 'react-star-ratings';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    fontSize: '15px',
+    fontFamily: 'consolas',
+    color: theme.palette.text.primary,
+}));
+
 const styles = {
     center_align: {
         display: 'flex',
@@ -15,15 +30,47 @@ const styles = {
         margin: '25px 0px 0px 0px',
         display: 'flex',
         justifyContent: 'center',
+    },
+    left_align: {
+        margin: '25px 0px 0px 0px',
+        display: 'flex',
+        justifyContent: 'left',
+    },
+    box_background: {
+        backgroundColor: 'white',
+        border: '1px solid black',
+        borderTopLeftRadius: '5px',
+        borderTopRightRadius: '5px',
+        borderBottomLeftRadius: '5px',
+        borderBottomRightRadius: '5px'
     }
 }
 
 export default function ReviewAndRatingPage({ userId }) {
 
     const navigate = useNavigate();
-    const [rating, setRating] = useState({'ratingAsSender': 0, 'ratingAsTraveler': 0});
+    const [rating, setRating] = useState({
+        'userId': userId,
+        'ratingAsSender': 0,
+        'ratingAsTraveler': 0
+    });
+    const [contract, setContract] = useState({
+        'userId': userId,
+        'totalContractAsSender': 0,
+        'totalDeliveredAsSender': 0,
+        'totalUnsuccessfulAsSender': 0,
+        'totalContractAsTraveler': 0,
+        'totalDeliveredAsTraveler': 0,
+        'totalUnsuccessfulAsTraveler': 0
+    });
 
     useEffect(() => {
+
+        if (!userId) {
+            console.log("User id is null.");
+            return;
+        }
+
         console.log("ReviewRating User ID: " + userId);
         AuthService.setAxiosAuthHeader();
         // localhost:8080/api/users/getRatingByUserId?userId=3
@@ -32,18 +79,50 @@ export default function ReviewAndRatingPage({ userId }) {
                 userId: Number(userId)
             }
         })
-        .then(res => {
-            setRating(res.data);
-            console.log("Rating As Sender = " + rating.ratingAsSender);
-            console.log("Rating As Traveler = " + rating.ratingAsTraveler);
-        })
-        .catch((err) => {
-            console.log(err);
-            if (err.response.status === 401) {
-                navigate(urlPaths.login);
+            .then(res => {
+                setRating(res.data);
+                console.log("Rating As Sender = " + rating.ratingAsSender);
+                console.log("Rating As Traveler = " + rating.ratingAsTraveler);
+            })
+            .catch((err) => {
+                console.log(err);
+                if (err.response.status === 401) {
+                    navigate(urlPaths.login);
+                }
+            })
+
+    }, [userId]);
+
+
+    useEffect(() => {
+
+        if (!userId) {
+            console.log("User id is null.");
+            return;
+        }
+
+        console.log("ReviewRating User ID: " + userId);
+        AuthService.setAxiosAuthHeader();
+
+        // localhost:8080/api/users/getContractFrequency?userId=3
+        axios.get("/api/users/getContractFrequency", {
+            params: {
+                userId: Number(userId)
             }
         })
-        
+            .then(res => {
+                setContract(res.data);
+                console.log("User id " + contract.userId);
+                console.log("Sender " + contract.totalContractAsSender);
+                console.log("Traveler " + contract.totalContractAsTraveler);
+            })
+            .catch((err) => {
+                console.log(err);
+                if (err.response.status === 401) {
+                    navigate(urlPaths.login);
+                }
+            })
+
     }, [userId]);
 
     return (
@@ -81,6 +160,48 @@ export default function ReviewAndRatingPage({ userId }) {
                     </div>
                 </div>
             </div>
+
+            <br />
+
+            <Box sx={{ flexGrow: 1 }} style={styles.box_background}>
+                <Grid container spacing={1}>
+                    <Grid item xs={6} md={12}>
+                        <Item>
+                            <label style={styles.center_align}>As sender</label>
+                            Number of contracts: {contract.totalContractAsSender}
+                            <Grid container spacing={1}>
+                                <Grid item xs={6} md={6}>
+                                    <Item>Delivered: {contract.totalDeliveredAsSender}</Item>
+                                </Grid>
+                                <Grid item xs={6} md={6}>
+                                    <Item>Unsuccessful: {contract.totalUnsuccessfulAsSender}</Item>
+                                </Grid>
+                            </Grid>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <br />
+
+            <Box sx={{ flexGrow: 1 }} style={styles.box_background}>
+                <Grid container spacing={1}>
+                    <Grid item xs={6} md={12}>
+                        <Item>
+                            <label style={styles.center_align}>As traveler</label>
+                            Number of contracts: {contract.totalContractAsTraveler}
+                            <Grid container spacing={1}>
+                                <Grid item xs={6} md={6}>
+                                    <Item>Delivered: {contract.totalDeliveredAsTraveler}</Item>
+                                </Grid>
+                                <Grid item xs={6} md={6}>
+                                    <Item>Unsuccessful: {contract.totalUnsuccessfulAsTraveler}</Item>
+                                </Grid>
+                            </Grid>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </>
     );
 }
