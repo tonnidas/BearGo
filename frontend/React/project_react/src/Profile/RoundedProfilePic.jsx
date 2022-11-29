ea1ef627be58e6f473940b3060529f4011738342
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-11-29 16:41:54 -0600

    Added image upload from user profile.

--- frontend/React/project_react/src/Profile/RoundedProfilePic.jsx
+++ frontend/React/project_react/src/Profile/RoundedProfilePic.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactRoundedImage from "react-rounded-image";
 import MyPhoto from "./profile_pic.jpg";
+import axios from 'axios';
+import AuthService from '../Service/AuthService';
 
 const styles = {
     profile_pic: {
@@ -29,16 +31,90 @@ const styles = {
     center_align: {
         display: 'flex',
         justifyContent: 'center'
+    },
+    left_align: {
+        display: 'flex',
+        justifyContent: 'left'
+    },
+    right_align: {
+        display: 'flex',
+        justifyContent: 'right'
     }
 }
 
+
 export default function RoundedProfilePic({ username, isOwnProfile }) {
+
+    const [inputs, setInputs] = React.useState({ ['imageUploadFile']: MyPhoto });
+
+    const handleUpload = async (event) => {
+        const name = event.target.name;
+        if (name === 'imageFile' && event.target.files && event.target.files[0]) {
+            const file = event.target.files[0];
+            const value = URL.createObjectURL(file);
+            setInputs(values => ({ ...values, [name]: value, ['imageUploadFile']: file }));
+        }
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!inputs.imageUploadFile) {
+            alert('Please select an image');
+            return;
+        }
+        AuthService.setAxiosAuthHeader();
+        try {
+            // first upload the image and get image id
+            let formData = new FormData();
+            formData.append('file', inputs.imageUploadFile);
+            const respImageUpload = await axios.post('/api/images/upload', formData);
+            console.log('image id:' + respImageUpload.data.id);
+
+            // localhost:8080/api/users/updateProfileImage/3
+            const resp = await axios.post('/api/users/updateProfileImage/' + respImageUpload.data.id);
+            console.log(resp.data);
+            alert('Profile image updated');
+        } catch (error) {
+            console.log(error);
+            alert('Failed to update profile image, reason: ' + error.response.data.message);
+        }
+    }
+
+    function getUploadImageOption(isOwnProfile) {
+        if (isOwnProfile == false) {
+            return (<></>);
+        }
+        return (
+            <>
+                <br />
+                <div className='row'>
+                    <div className='col-md-6' style={styles.right_align}>
+                        <div className='post' style={styles.center_align}>
+                            <label className='upload-btn'>
+                                Select
+                                <input type='file' single name='imageFile' onChange={handleUpload} />
+                            </label>
+                        </div>
+                    </div>
+                    <div className='col-md-6' style={styles.left_align}>
+                        <div className='post' style={styles.center_align}>
+                            <label className='upload-btn'>
+                                Upload
+                                <input type='file' single name='imageFile' onClick={handleSubmit} />
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+
     return (
         <>
             <div style={styles.div_background}>
                 <div style={styles.profile_pic}>
                     <ReactRoundedImage
-                        image={MyPhoto}
+                        image={inputs.imageFile || MyPhoto}
                         roundedColor="#321124"
                         imageWidth="150"
                         imageHeight="150"
@@ -51,23 +127,6 @@ export default function RoundedProfilePic({ username, isOwnProfile }) {
                 <div style={styles.profile_pic_name}>
                     <span style={styles.profile_pic_name_style}>{username}</span>
                 </div>
-            </div>
-        </>
-    );
-}
-
-function getUploadImageOption(isOwnProfile) {
-    if (isOwnProfile == false) {
-        return (<></>);
-    }
-    return (
-        <>
-            <br />
-            <div className='post' style={styles.center_align}>
-                <label className='upload-btn'>
-                    <i className='icon-upload'></i> Update Image
-                    <input type='file' single name='imageFile' />
-                </label>
             </div>
         </>
     );
