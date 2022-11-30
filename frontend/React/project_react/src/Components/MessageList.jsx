8d38d7dcf2f7b5dff517173e1858cc4529394d8a
agmislam1 <104467848+agmislam1@users.noreply.github.com>
2022-11-29 19:34:47 -0600

    message front end added

--- frontend/React/project_react/src/Components/MessageList.jsx
+++ frontend/React/project_react/src/Components/MessageList.jsx
@@ -1,32 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 export const MessageList = (props) => {
   return (
-    <>    <div className='body'>
-
-      <div className='search'>
-        <input placeholder='Search...' type='text' />
+    <>
+      <div className='body'>
+        <div className='search'>
+          <input placeholder='Search...' type='text' />
+        </div>
+        <ul>
+          {
+            props.data.map(chatObj => {
+              const userId = chatObj.fromid === Number(props.id) ? chatObj.toid : chatObj.fromid;
+              const userName = chatObj.fromid === Number(props.id) ? chatObj.toName : chatObj.fromname;
+              return (<li>
+                <a className='thumbnail' href='#'>
+                  {userName.substring(0, 1)}
+                </a>
+                <div className='content'>
+                  <h3>{userName}</h3>
+                  <span className='preview'><a href='#'>{chatObj.msg ? chatObj.msg.substring(0, 10) + '...' : ''}</a></span>
+                </div>
+              </li>
+              );
+            })
+          }
+        </ul>
       </div>
-      <ul>
-
-        {
-          props.data.map(chatObj => {
-            const userId = chatObj.fromUser === Number(props.id) ? chatObj.toUser : chatObj.fromUser;
-            console.log(userId, Number(props.id));
-            return (<li>
-              <a className='thumbnail' href='#'>
-                NR
-              </a>
-              <div className='content'>
-                <h3>{userId}</h3>
-                <span className='preview'><a href='#'>{chatObj.msg ? chatObj.msg.substring(0, 10) + '...' : ''}</a></span>
-              </div>
-            </li>
-            );
-          })
-        }
-      </ul>
-    </div>
     </>
   )
 }
