381db6ae129ef78c5b6bc3c9710c580c7fe53796
Swapnil Saha <58855464+swapnilsaha-me@users.noreply.github.com>
2022-12-01 04:27:45 -0600

    Added backend API for getAllComplaintByUserId.

--- backend/src/main/java/edu/baylor/cs/beargo/repository/UserComplaintRepository.java
+++ backend/src/main/java/edu/baylor/cs/beargo/repository/UserComplaintRepository.java
@@ -4,7 +4,9 @@ import edu.baylor.cs.beargo.model.UserComplaint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserComplaintRepository extends JpaRepository<UserComplaint, Long> {
-
+    List<UserComplaint> findByComplainedUserId(Long userId);
 }
