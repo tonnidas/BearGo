8bd36597f30248c49cafe00894c67153979e5cd3
agmislam1 <104467848+agmislam1@users.noreply.github.com>
2022-11-30 21:46:17 -0600

    twitter streaming application added

---
+++ twitter-stream-integration/src/main/java/com/saad056/twitterstreamintegration/TwitterStreamIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.saad056.twitterstreamintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterStreamIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterStreamIntegrationApplication.class, args);
+    }
+
+}
---
+++ twitter-stream-integration/src/test/java/com/saad056/twitterstreamintegration/TwitterStreamIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad056.twitterstreamintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterStreamIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
